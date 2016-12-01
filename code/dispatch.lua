9b786d8f25e1e53ee34da03256d6ac6c956d4416
Tung <me@tungv.com>
2016-12-01 19:10:14 +0700

    second commit

--- code/dispatch.lua
+++ code/dispatch.lua
@@ -1,15 +1,19 @@
-local function match(t)
-  t.case = function (self, state, action)
-    local f = self[action.type] or self.default
-    if f then
-      if type(f)=="function" then
-        return f(state, action,self)
-      else
-        error("handler for "..tostring(action.type).." not a function")
-      end
+local function identity(x) return x end;
+
+local function handleActions(actions, initialState)
+  return function (state, action)
+    if state == nil then
+      state = initialState;
+    end
+
+    local f = actions[action.type] or identity
+
+    if type(f) == "function" then
+      return f(state, action)
+    else
+      error("handler for "..tostring(action.type).." not a function")
     end
   end
-  return t
 end
 
 local function concat(t1, t2)
@@ -20,23 +24,16 @@ local function concat(t1, t2)
   return r
 end
 
-local function reducer (state, action)
-  if state == nil then
-    return {
-      list = { 1, 2, 3 },
-      length = 3
-    }
+local initialState = {
+  list = { 1, 2, 3 },
+  length = 3
+}
+
+local reducer = handleActions({
+  PUSH = function (state, action)
+    return { length = state.length + 1, list = concat(state.list, {action.payload}) }
   end
-
-  local matcher = match {
-    PUSH = function (state, action)
-      return { length = state.length + 1, list = concat(state.list, {action.payload}) }
-    end,
-    default = function (state) return state end
-  }
-
-  return matcher:case(state, action);
-end
+}, initialState)
 
 local function dispatch (action)
   local storeKey = KEYS[1] .. '_test'
